35a092a4edc9f02e516104a554fe3b4fdd9f0cdb
Musho810 <musho810@gmail.com>
2022-10-02 20:18:06 +0400

    Initial commit

---
+++ src/main/java/am/itspace/overnight/OvernightApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.overnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvernightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvernightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/itspace/overnight/OvernightApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.overnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvernightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
