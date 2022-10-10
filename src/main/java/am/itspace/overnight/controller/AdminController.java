7356bc62f616ad945f310be3e1e69c6a5992a881
Hovsep Mkrtchyan <hovsepmkrtchyan15@gmail.com>
2022-10-10 17:06:36 +0400

    admin html and controller

---
+++ src/main/java/am/itspace/overnight/controller/AdminController.java
@@ -0,0 +1,12 @@
+package am.itspace.overnight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("adminPage")
+    public String adminpage(){
+        return "/adminPage";
+    }
+}
