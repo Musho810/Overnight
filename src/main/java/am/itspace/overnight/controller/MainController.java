629a34c30aaaed49e95b8c356db987142b3def80
Gevorg Sargsyan <gyumriparts@gmail.com>
2022-10-17 22:53:30 +0400

    index page i18n

---
+++ src/main/java/am/itspace/overnight/controller/MainController.java
@@ -0,0 +1,13 @@
+package am.itspace.overnight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+}
