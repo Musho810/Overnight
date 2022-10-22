f116f4a00f2b3804a229128c8d9c801d9855b2e9
Hovsep Mkrtchyan <hovsepmkrtchyan15@gmail.com>
2022-10-22 21:15:03 +0400

    admin pages

--- src/main/java/am/itspace/overnight/controller/UserController.java
+++ src/main/java/am/itspace/overnight/controller/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("user")
-    public String user(){
-        return "user";
-    }
+//    @GetMapping("user")
+//    public String user(){
+//        return "user";
+//    }
 }
--- src/main/java/am/itspace/overnight/repository/RegionRepository.java
+++ src/main/java/am/itspace/overnight/repository/RegionRepository.java
@@ -4,4 +4,6 @@ import am.itspace.overnight.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegionRepository extends JpaRepository<Region,Integer> {
+
+
 }
