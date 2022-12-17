710ad3ca8e60b34673a9ce93f6776883a93fd4a1
Gevorg Sargsyan <gyumriparts@gmail.com>
2022-12-18 01:04:05 +0400

    fine

--- src/main/java/am/itspace/overnight/controller/RoomController.java
+++ src/main/java/am/itspace/overnight/controller/RoomController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
